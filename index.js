1a8a97c3dbd85ac1771fd804071655107a3a5acc
Maya_Khaled <may2yk@yahoo.com>
2021-09-03 20:32:19 +0200

    last commit with cors

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
-const mongoose = require('mongoose')
-const connectDB = require('./database')
+const mongoose = require("mongoose");
+const connectDB = require("./database");
+const cors = require("cors");
 
-const express = require('express')
-const app = express()
-const usersRouter = require('./Routers/usersRouter')
-const todoRouter = require('./Routers/todoRouter')
-const notesRouter = require('./Routers/notesRouter')
-const todolistRouter = require('./Routers/todolistRouter')
-app.use(express.json())
+const express = require("express");
+const app = express();
+const usersRouter = require("./Routers/usersRouter");
+const todoRouter = require("./Routers/todoRouter");
+const notesRouter = require("./Routers/notesRouter");
+const todolistRouter = require("./Routers/todolistRouter");
+app.use(express.json());
+app.use(cors());
+app.use("/users", usersRouter);
+app.use("/notes", notesRouter);
+app.use("/todo", todoRouter);
+app.use("/todolist", todolistRouter);
 
-app.use('/users', usersRouter)
-app.use('/notes', notesRouter)
-app.use('/todo', todoRouter)
-app.use('/todolist' , todolistRouter)
+app.listen(8000);
 
-app.listen(3000)
-
-connectDB()
+connectDB();
